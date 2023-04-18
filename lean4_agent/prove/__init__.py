df74e2c3d9e42ef542ec1602c903afb762b80b4d
zhangir-azerbayev <zazerbayev@gmail.com>
2023-04-18 18:17:59 -0400

    wip

--- lean4_agent/prove/__init__.py
+++ lean4_agent/prove/__init__.py
@@ -2,19 +2,43 @@ from lean4_agent.util import CONFIG
 from lean4_agent.llm import *
 from lean4_agent.gym import ProofSearch
 
+from dataclasses import dataclass
+from typing import List
+
 from pydantic import BaseModel
 
 import tiktoken
 
+@dataclass 
+class Sorry: 
+    line: int
+    column: int
+    text: int
 
-def code_of_chat_state(state: ChatState):
+@dataclass Goal: 
+    text: int 
+
+@dataclass Error: 
+    text: int
+
+@dataclass
+class ProverState: 
+    sketch: str
+    code: str 
+    sorries: List[Sorry]
+    goals: List[Goal]
+    errors: List[Error]
+
+def code_of_chat_state(state: ChatState, lang="lean"):
     """
     Extracts contents of last lean code block from raw text
 
     Requires that ChatState.messages[-1] has role "assistant"
     """
+    assert state.messages[-1].role == "assistant"
+
     text = state.messages[-1].content
-    left_key = "```lean"
+    left_key = f"```{lang}"
     left_idx = text.rindex(left_key)
     right_idx = text.rindex("```")
     code = text[left_idx + len(left_key) + 1 : right_idx]  # +1 for the newline
@@ -27,6 +51,148 @@ def sorries_goals_errors_of_lean_state(lean_state):
     errors = [m for m in lean_state["messages"] if "unsolved goals" not in m["data"]]
     return sorries, goals, errors
 
+def sketch_prompt(code: str) -> str: 
+    return f"""\
+I am trying to write a proof of the following theorem in Lean 4: 
+```lean
+{code}
+```
+I am going to ask you to complete this formal proof. But first, plan out your formal proof in natural language and LaTeX. 
+
+Formatting instructions: enclose your plan in a ```code block```.\
+"""
+
+def sketch_of_code(code: str) -> str: 
+    user_prompt = sketch_prompt(code)
+
+    chat_state = ChatState(
+        messages=[
+            ChatMessage(role="system", content=SYSTEM_MESSAGE),
+            ChatMessage(role="user", content=user_prompt),
+        ]
+    )
+
+    chat_state = complete_chat(chat_state)
+
+    sketch = code_of_chat_state(chat_state, lang="")
+    return sketch
+
+def prove_prompt(proverstate: ProverState) -> str: 
+    return f"""\
+I am trying to complete this proof in Lean 4: 
+```lean
+{proverstate.code}
+```
+
+I plan to proceed according to this natural language proof sketch: 
+
+{proverstate.sketch}
+
+The following are the open goals of my code: 
+```
+{"\n\n".join([x.text for x in proverstate.goals])}
+```
+1. Please write out a plan for completing the formal proof. Write your plan in English (with LaTeX).
+2. Please add the next tactic step to the proof. 
+
+Formatting instructions: include the new version of your (possibly incomplete) proof in a lean code block. Make sure the code block is self-contained and runs. Do not add more than one new tactic step. Do not write `sorry` in your code. Instead, stop writing where you think it will be helpful for me to see the goal state. 
+"""
+
+def fix_error_prompt(proverstate: ProverState) -> str: 
+    return f"""\
+The following is a Lean 4 proof I am working on: 
+
+```lean
+{proverstate.code}
+```
+
+I am following this proof sketch: 
+
+{proverstate.sketch}
+
+This proof returns the following errors. 
+```
+{"\n\n".join([x.text for x in proverstate.errors])}
+```
+
+Please describe how you are going to fix the error. Modify the code to fix the error, but do not add any additional tactic steps. 
+
+Formatting instructions: Write the answer in a `lean` code block. Do not include any sorries in your modified code. Rather, finish writing where you want me to see the goal state.\
+"""
+
+def prover_kernel(proverstate: ProverState, mode: str, verbose=False) -> ProverState: 
+    """
+    Takes a ProverState with proverstate.goals nonempty, and prompts a language model 
+    to fix the error.
+
+    Args: 
+        proverstate (ProverState)
+        mode (str): equal to "prove" or "error"
+
+    Requires: 
+        if `mode="prover"`, requires `not proverstate.sorries`
+        if `model="error"`, requires `proverstate.sorries`
+    """
+    if mode="prover":
+        assert not proverstate.errors
+    elif model="error": 
+        assert proverstate.errors
+    else: 
+        raise ValueError("`mode` not recognized")
+
+    user_prompt = fix_error_prompt(proverstate)
+
+    chat_state = ChatState(
+        messages=[
+            ChatMessage(role="system", content=SYSTEM_MESSAGE),
+            ChatMessage(role="user", content=user_prompt),
+        ]
+    )
+
+    chat_state = complete_chat(chat_state)
+
+    if verbose: 
+        print(f">>>{mode} MODE")
+        print(">>>USER")
+        print(chat_state.messages[-2].content)
+        print(">>>ASSISTANT")
+        print(chat_state.messages[-1].content)
+
+    new_code = code_of_chat_state(chat_state).strip()
+
+    replpath = os.environ.get("PATH_TO_LEAN_REPL")
+    lean = ProofSearch(replpath)
+
+    lean_state = lean.run_code(new_code, verbose=verbose)
+    lean_states.append(lean_state)
+
+    sorries, goals, errors = sorries_goals_errors_of_lean_state(lean_state)
+
+    if sorries: 
+        raise ValueError("haven't implemented coping with sorries yet")
+
+    new_proverstate = ProverState(
+            sketch=proverstate.sketch, 
+            code=new_code, 
+            goals=goals, 
+            errors=errors,
+    )
+
+    if verbose: 
+        print(new_proverstate)
+
+    return new_proverstate, chat_state
+
+def prover(proverstate: ProverState, max_api_calls=10, verbose=False) -> Dict: 
+    pass
+
+def f2f_prove(code: str, **kwargs) -> Dict: 
+    pass
+    
+
+"""
+below this line is all old stuff
+"""
 
 def calc_tokens(chat_state: ChatState, model="gpt-4"):
     enc = tiktoken.encoding_for_model(model)
