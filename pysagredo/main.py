11cd2fc7fdebbef434d7e4ee1a2a03148027b2dd
zhangir-azerbayev <zazerbayev@gmail.com>
2023-05-03 18:11:54 -0400

    minif2f

--- pysagredo/main.py
+++ pysagredo/main.py
@@ -3,6 +3,9 @@ import os
 import sys
 import time
 import json
+from pathlib import Path
+from tqdm import tqdm
+from itertools import islice
 
 from pysagredo.util import CONFIG
 from pysagredo.llm import *
@@ -12,6 +15,7 @@ from pysagredo.prove import f2f_prove, autoformalize_sketch
 from pydantic import BaseModel
 
 import tiktoken
+from datasets import load_dataset
 
 import code 
 
@@ -26,28 +30,35 @@ def test_proofsearch(source):
     print(out)
     print("repl worked...")
 
-def _main():
+def _main(args):
     """
     Only for debugging/testing purposes
     """
-
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-a', '--autoformalize', action='store_true')
-    parser.add_argument('-p', '--prove', action='store_true')
-    parser.add_argument('-i', '--input', type=str, help="where to look for input file")
-    args = parser.parse_args()
 
     path = args.input
 
     start_time = time.time()
 
-    if args.prove: 
+    if args.prove and path!="minif2f": 
         source = open(path).read() 
         test_proofsearch(source)
-        summary = f2f_prove(source, max_api_calls=10, verbose=True) 
-    
-    if args.autoformalize: 
+        summary = f2f_prove(source, max_api_calls=10, verbose=True)  
+
+    elif args.prove and path=="minif2f": 
+        Path(args.logdir).mkdir(parents=True)
+        dataset = load_dataset("hoskinson-center/minif2f-lean4", split="validation")
+        for x in tqdm(islice(dataset, 2)): 
+            print(x)
+            eyed = x["id"]
+            source = x["header"] + "\n\n" + x["formal_statement"]
+            test_proofsearch(source) # sanity check to make sure repl works
+            summary = f2f_prove(source, max_api_calls=10, verbose=True)  
+            
+            print(f"saving summary for {id}...")
+            with open(args.logdir, "w") as f: 
+                json.dump(summary, f)
+                
+    elif args.autoformalize: 
         source = json.load(open(path))
         test_proofsearch(source)
         sketch = source["sketch"]
@@ -66,4 +77,11 @@ def _main():
     print(program)
 
 if __name__ == "__main__":
-    _main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-a', '--autoformalize', action='store_true')
+    parser.add_argument('-p', '--prove', action='store_true')
+    parser.add_argument('-i', '--input', type=str, help="where to look for input file")
+    parser.add_argument('-l', '--logdir', type=str, help="directory you want to save logs")
+    args = parser.parse_args()
+    _main(args)
